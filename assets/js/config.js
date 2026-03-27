0e51179fac9acd69d664d31129634847f9522a28
cyotamo <cyotamo@yahoo.com.br>
2026-03-27 19:37:09 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 (function () {
-  window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyn6lK5pfr55HiXu5rbaNNNUknrG6M020Mf16D3IPcru5Kmx0jULbJshTkHR_rfCRX_/exec';
+  window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwc0FAKjJrYPPzibclriliPgmKLugxPKpanp4baZxQlcQ05JRtU-uFJy-qkC-2ky7FO/exec';
 }());
